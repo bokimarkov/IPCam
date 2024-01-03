3959024a65cb59a1cbd45855a2c858fca3ceffe8
bokimarkov <116968074+bokimarkov@users.noreply.github.com>
2024-01-03 11:29:23 +0100

    Update main.js

--- main.js
+++ main.js
@@ -10,7 +10,9 @@ const server = http.createServer(app);
 const WebSocket = require("ws");
 
 let keepAliveId;
-
+let visitorCount = {
+    "count": 0
+}
 const wss =
   process.env.NODE_ENV === "production"
     ? new WebSocket.Server({ server })
@@ -22,6 +24,7 @@ console.log(`Server started on port ${serverPort} in stage ${process.env.NODE_EN
 wss.on("connection", function (ws, req) {
   console.log("Connection Opened");
   console.log("Client size: ", wss.clients.size);
+  visitorCount.count ++
 
   if (wss.clients.size === 1) {
     console.log("first connection. starting keepalive");
@@ -71,10 +74,10 @@ const broadcast = (ws, message, includeSelf) => {
   keepAliveId = setInterval(() => {
     wss.clients.forEach((client) => {
       if (client.readyState === WebSocket.OPEN) {
-        client.send('ping');
+        client.send(visitorCount.count);
       }
     });
-  }, 50000);
+  }, 1000);
 };
 
 
